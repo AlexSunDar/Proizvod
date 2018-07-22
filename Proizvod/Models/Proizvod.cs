4b42bbb4e4f2767fcbf9f8557dcccb664d9a3e72
Аца <thesmilingcloud@gmail.com>
2018-07-22 04:46:19 +0200

    First commit

---
+++ Proizvod/Models/Proizvod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proizvod.Models
+{
+    public class Proizvod
+    {
+        private int Id { get; set; }
+        private string Naziv { get; set; }
+        private string Opis { get; set; }
+        private string Kategorija { get; set; }
+        private double Cena { get; set; }
+    }
+}
